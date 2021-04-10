0a8d2557e93d7401389644d5ac7fe2b42016d810
i'mfebri <iamfebriadji@gmail.com>
2021-04-11 00:26:45 +0700

    menambahkan path router untuk component home dan dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,23 @@
+import { Fragment } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+// import semua halaman
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 export default function App() {
-  return <Login />
+  return (
+    <Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={ Login } />
+          <Route exact path="/register" component={ Register } />
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/:username" component={ Dashboard } />
+        </Switch>
+      </BrowserRouter>
+    </Fragment>
+  )
 }
