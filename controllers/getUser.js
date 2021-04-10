a335c4e809d3927d4f254b6cc57054bd627c1f75
i'mfebri <iamfebriadji@gmail.com>
2021-04-11 00:54:23 +0700

    menambahkan controllers untuk mengambil data user

---
+++ controllers/getUser.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Users = require('../models/users')
+
+exports.routerGetUser = async (req, res, next) => {
+  try {
+    const _id = req.user.user_id // mengambil id pengguna
+
+    // mencari data pengguna di database
+    await Users.findOne({ _id }, (err, user) => {
+      if (err) throw err
+      
+      res.status(200).json(user) // mengirim data user ke client
+    })
+  }
+  catch(err) {
+    console.error(err)
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,11 @@ const
 const 
   { routerRegister } = require('../controllers/register')
 , { routerLogin } = require('../controllers/login')
+, { routerGetUser } = require('../controllers/getUser')
 
 router.post('/login', routerLogin) // router untuk aksi login
 router.post('/register', routerRegister) // router untuk aksi register
+
+router.get('/user', tokenAuthenticate, routerGetUser) // router untuk mengambil data user
 
 module.exports = router
