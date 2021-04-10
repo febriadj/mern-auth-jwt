b5c35ec803a52b7705ca2a6b109da78696d8bb76
i'mfebri <iamfebriadji@gmail.com>
2021-04-11 00:28:58 +0700

    menambahkan kondisi untuk mengecek token otentikasi pengguna

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 export default function Dashboard() {
+  const isLoggedIn = localStorage.getItem('token')
+
+  if (!isLoggedIn) {
+    return <Redirect to="/login" />
+  }
+
   return (
     <div className="dashboard">
       <div className="dashboard-wrap">
