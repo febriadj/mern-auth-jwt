bfdae6ebdd33af50ef11f8398ba57f3da443e11d
i'mfebri <iamfebriadji@gmail.com>
2021-04-11 00:27:55 +0700

    import containers form register

---
+++ client/src/pages/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormRegister from '../containers/FormRegister'
+
+export default function Register() {
+  return (
+    <div className="login">
+      <div className="login-wrap">
+        <FormRegister />
+      </div>
+    </div>
+  )
+}
