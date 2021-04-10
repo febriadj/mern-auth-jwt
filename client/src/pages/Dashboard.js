cd71d8379cd363275b725c3b4e0b16e91d87d979
i'mfebri <iamfebriadji@gmail.com>
2021-04-11 02:35:53 +0700

    mengirim token melalui headers dan menampilkan data user

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Redirect } from 'react-router-dom'
 
 export default function Dashboard() {
-  const isLoggedIn = localStorage.getItem('token')
+  const isLoggedIn = localStorage.getItem('token') // mengambil token pada local storage
+  const [userData, setUserData] = useState({}) // state ini untuk menyimpan data pengguna
 
+  useEffect(() => {
+    (async function fetchData() {
+      const fetchUser = await fetch('http://localhost:8080/api/user', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + isLoggedIn // memberikan token ke server
+        }
+      })
+      
+      const userJson = fetchUser.json() // mengambil data fetch dalam bentuk json
+      const user = await userJson // userJson berbentuk promise maka harus di await
+  
+      return setUserData(user)
+    })()
+  }, [isLoggedIn])
+  
+  // kondisi jika tidak ada token didalam local storage
   if (!isLoggedIn) {
-    return <Redirect to="/login" />
+    return <Redirect to="/login" /> //re-redirect kehalaman login
   }
 
   return (
     <div className="dashboard">
       <div className="dashboard-wrap">
-        <h2>Halaman Dashboard</h2>
+        <h2 className="username">{ userData.username }</h2>
+        <p className="email">{ userData.email }</p>
       </div>
     </div>
   )
