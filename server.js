bb0f9ac28d0b35194e4636f76be233b1434f46b9
i'mfebri <iamfebriadji@gmail.com>
2021-04-26 01:16:10 +0700

    menambahkan kondisi production

--- server.js
+++ server.js
@@ -18,5 +18,13 @@ app.use('/api', require('./routes/index')) // semua rute program
 
 require('./config/database') // menjalankan mongodb server
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 app.listen(port)
 console.log('server running on port:' + port)
