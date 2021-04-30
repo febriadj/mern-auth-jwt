d1d01379cded49f83123cfd08c703581f5eccffc
i'mfebri <iamfebriadji@gmail.com>
2021-04-30 22:39:50 +0700

    update path join __dirname pada routing client

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
   
   app.get('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '/client', '/build', '/index.html'))
+    res.sendFile(path.join(__dirname, '/client', '/build', 'index.html'))
   })
 }
 
