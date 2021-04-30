e07d08fc684983c81ef79e727f073578442cd669
i'mfebri <iamfebriadji@gmail.com>
2021-04-30 22:19:14 +0700

    update path join __dirname pada routing client

--- server.js
+++ server.js
@@ -17,15 +17,16 @@ app.use(express.json())
 
 app.use('/api', require('./routes/index')) // semua rute program
 
-require('./config/database') // menjalankan mongodb server
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
-
+  
   app.get('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '/client', '/build', '/index.html'))
   })
 }
+
+require('./config/database') // menjalankan mongodb server
 
 app.listen(port)
 console.log('server running on port:' + port)
